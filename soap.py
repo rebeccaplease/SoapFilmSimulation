b31ebfe2f06c729e82af4a332df9ee8b7068e363
Rebecca <rebeccaplease@gmail.com>
2015-11-20 00:52:07 -0500

    added constants

---
+++ soap.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function
+from math import pi
+
+#Constants
+#parameters used in the paper
+g = 9.8         #gravity acceleration (m/s^2)
+rho = 1.1       #density of solution (g/cm^3)
+gamma = 0.042   #surface tension (N/m)
+mu = 2          #viscosity (cP)
+mu_0 = pi*4E-7  #magnetic permeability (N/A^2)
+B_0 =  1684     #magnetic field strength (G)
+chi_m = 0.05    #magnetic susceptibility
+R = 4.4         #radius of current loop (cm)
+h_b = 1         #film thickness at edge (mm)
+
+
+#Boundary conditions
